5a2aac280bf5b810c790c5c74f94318e492221fd
Leong Wai Yin <waiyin@pulseid.com>
2019-06-30 23:56:25 +0800

    Move App into component folder

--- src/index.jsx
+++ src/index.jsx
@@ -1,12 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-class App extends React.Component{
-    render(){
-        return(
-            <div>Hello World</div>
-        )
-    }
-}
+import App from './components/App';
 
 ReactDOM.render(<App />, document.getElementById('app'))
