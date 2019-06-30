22024f7322d28d50b10f90ea12fcd38e0c81e9f5
Leong Wai Yin <waiyin@pulseid.com>
2019-06-30 23:53:42 +0800

    Update webpack to split module chunks

--- webpack.config.js
+++ webpack.config.js
@@ -14,15 +14,41 @@ module.exports = {
 
   module : {
     rules : [
-      {test : /\.(js)$/, use:'babel-loader'},
+      {test : /\.(jsx|js)$/, use:'babel-loader'},
       {test : /\.css$/, use:['style-loader', 'css-loader']}
     ]
   },
 
+  // Reference: https://hackernoon.com/the-100-correct-way-to-split-your-chunks-with-webpack-f8a9df5b7758
   optimization: {
     splitChunks: {
       chunks: 'all'
-    }
+    },
+
+    runtimeChunk: 'single',
+
+    splitChunks: {
+      chunks: 'all',
+      maxInitialRequests: Infinity,
+      minSize: 0,
+      cacheGroups: {
+        vendor: {
+          test: /[\\/]node_modules[\\/]/,
+          name(module) {
+            // get the name. E.g. node_modules/packageName/not/this/part.js
+            // or node_modules/packageName
+            const packageName = module.context.match(/[\\/]node_modules[\\/](.*?)([\\/]|$)/)[1];
+
+            // npm package names are URL-safe, but some servers don't like @ symbols
+            return `npm.${packageName.replace('@', '')}`;
+          },
+        },
+      },
+    },
+  },
+
+  resolve: {
+    extensions: ['.js', '.jsx'],
   },
 
   plugins: [
