b17988302c087a2318eee00f892714a8e8ccf426
LWY <gashzack@hotmail.com>
2019-07-07 16:08:18 +0900

    Add delay in GET /user

--- packages/server/src/index.js
+++ packages/server/src/index.js
@@ -8,10 +8,13 @@ app.get('/', (req, res) => {
 });
 
 app.get('/user', (req, res) => {
-  res.json({
-    name: 'akira',
-    email: 'hasegawa@hotmail.com'
-  });
+  // simulate delay to test lazy-loading logic in client
+  setTimeout(() => {
+    res.json({
+      name: 'akira',
+      email: 'hasegawa@hotmail.com'
+    });
+  }, 1000);
 });
 
 server = app.listen(port, hostname, () => {
