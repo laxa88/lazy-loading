ddc6973f26ff8e8d7ced31e81d6fbfdc283673be
LWY <gashzack@hotmail.com>
2019-07-07 17:09:57 +0900

    Add babel polyfill, refactor Promise to async/await

--- packages/client/src/components/feature1/actions.js
+++ packages/client/src/components/feature1/actions.js
@@ -4,12 +4,12 @@ import { NAME } from './constants';
 
 export const [setName, setEmail] = createSetterActions(NAME, ['name', 'email']);
 
-export const fetchUser = createAction(`${NAME}/FETCH_USER`, () => dispatch => {
-  fetch('http://localhost:3000/user')
-    .then(data => data.json())
-    .then(json => {
-      const { name, email } = json;
-      dispatch(setName(name));
-      dispatch(setEmail(email));
-    });
-});
+export const fetchUser = createAction(
+  `${NAME}/FETCH_USER`,
+  () => async dispatch => {
+    const data = await fetch('http://localhost:3000/user');
+    const { name, email } = await data.json();
+    dispatch(setName(name));
+    dispatch(setEmail(email));
+  }
+);
--- packages/client/src/index.jsx
+++ packages/client/src/index.jsx
@@ -1,5 +1,6 @@
-import React from 'react';
+import '@babel/polyfill';
 import { Provider } from 'react-redux';
+import React from 'react';
 import ReactDOM from 'react-dom';
 
 import store from './reducer';
