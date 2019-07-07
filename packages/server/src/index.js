11aecf40ecb8130daaeee52466a9b1090350eec8
LWY <gashzack@hotmail.com>
2019-07-07 16:53:19 +0900

    Allow CORS on server

--- packages/server/src/index.js
+++ packages/server/src/index.js
@@ -3,6 +3,15 @@ const app = require('express')();
 const hostname = '127.0.0.1';
 const port = 3000;
 
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept'
+  );
+  next();
+});
+
 app.get('/', (req, res) => {
   res.send('Server is online.');
 });
