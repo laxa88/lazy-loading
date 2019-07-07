0c3b11cea4403fef169bd7601e938ed5a1120cf5
LWY <gashzack@hotmail.com>
2019-07-07 16:57:01 +0900

    Add fetchUser logic

--- packages/client/src/components/feature1/Feature1.jsx
+++ packages/client/src/components/feature1/Feature1.jsx
@@ -4,10 +4,11 @@ import { connect } from 'react-redux';
 import * as actions from './actions';
 import * as selectors from './selectors';
 
-const Feature1 = ({ name, email, setName, setEmail }) => (
+const Feature1 = ({ name, email, setName, setEmail, fetchUser }) => (
   <div>
     Hello, <input value={name} onChange={e => setName(e.target.value)} />! Your
     email is <input value={email} onChange={e => setEmail(e.target.value)} />.
+    <button onClick={() => fetchUser()}>Click to fetch user</button>
   </div>
 );
 
@@ -19,5 +20,6 @@ export default connect(
   {
     setName: actions.setName,
     setEmail: actions.setEmail,
+    fetchUser: actions.fetchUser,
   }
 )(Feature1);
--- packages/client/src/components/feature1/actions.js
+++ packages/client/src/components/feature1/actions.js
@@ -1,5 +1,15 @@
-import { createSetterActions } from '../../redux';
+import { createSetterActions, createAction } from '../../redux';
 
 import { NAME } from './constants';
 
 export const [setName, setEmail] = createSetterActions(NAME, ['name', 'email']);
+
+export const fetchUser = createAction(`${NAME}/FETCH_USER`, () => dispatch => {
+  fetch('http://localhost:3000/user')
+    .then(data => data.json())
+    .then(json => {
+      const { name, email } = json;
+      dispatch(setName(name));
+      dispatch(setEmail(email));
+    });
+});
